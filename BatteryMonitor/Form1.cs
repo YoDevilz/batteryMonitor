82573ffd93ecf7478626fd73816473de38d63194
Geereesen Mootoosamy <g.mootoosamy@outlook.com>
2019-10-08 14:07:48 +0400

    Add project files.

---
+++ BatteryMonitor/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatteryMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            strPowerLineStatus.Text = GetPowerSource();
+            batterylife.Text = GetBatteryStatus().ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public String GetPowerSource()
+        {
+            string strPowerLineStatus = "Default";
+            // Getting the current system power status.
+            switch (SystemInformation.PowerStatus.PowerLineStatus)
+            {
+                case PowerLineStatus.Offline:
+                    strPowerLineStatus = "Battery";
+                    break;
+                case PowerLineStatus.Online:
+                    strPowerLineStatus = "AC Power";
+                    break;
+                case PowerLineStatus.Unknown:
+                    strPowerLineStatus = "Unknown";
+                    break;
+            }
+            return strPowerLineStatus;
+        }
+        public float GetBatteryStatus()
+        {
+            float batterylife;
+            batterylife = SystemInformation.PowerStatus.BatteryLifePercent;
+            batterylife *= 100.0f;
+            return batterylife;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+    }
+   
+}
