197bcf24d2fedd2f0cd3c12a353a7654598e53a4
Geereesen Mootoosamy <g.mootoosamy@outlook.com>
2019-10-08 13:41:06 +0400

    add yo label

--- BatteryMonitor/Form1.Designer.cs
+++ BatteryMonitor/Form1.Designer.cs
@@ -36,15 +36,17 @@ namespace BatteryMonitor
             this.batterylife = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(61, 110);
+            this.label1.Location = new System.Drawing.Point(46, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 29);
+            this.label1.Size = new System.Drawing.Size(131, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Battery Status :";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -53,9 +55,10 @@ namespace BatteryMonitor
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(61, 213);
+            this.label2.Location = new System.Drawing.Point(46, 173);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(178, 29);
+            this.label2.Size = new System.Drawing.Size(140, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Power Source :";
             // 
@@ -63,9 +66,10 @@ namespace BatteryMonitor
             // 
             this.strPowerLineStatus.AutoSize = true;
             this.strPowerLineStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.strPowerLineStatus.Location = new System.Drawing.Point(268, 110);
+            this.strPowerLineStatus.Location = new System.Drawing.Point(201, 89);
+            this.strPowerLineStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.strPowerLineStatus.Name = "strPowerLineStatus";
-            this.strPowerLineStatus.Size = new System.Drawing.Size(88, 29);
+            this.strPowerLineStatus.Size = new System.Drawing.Size(67, 24);
             this.strPowerLineStatus.TabIndex = 2;
             this.strPowerLineStatus.Text = "Default";
             // 
@@ -73,17 +77,19 @@ namespace BatteryMonitor
             // 
             this.batterylife.AutoSize = true;
             this.batterylife.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.batterylife.Location = new System.Drawing.Point(268, 213);
+            this.batterylife.Location = new System.Drawing.Point(201, 173);
+            this.batterylife.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.batterylife.Name = "batterylife";
-            this.batterylife.Size = new System.Drawing.Size(88, 29);
+            this.batterylife.Size = new System.Drawing.Size(67, 24);
             this.batterylife.TabIndex = 3;
             this.batterylife.Text = "Default";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(59, 334);
+            this.button1.Location = new System.Drawing.Point(44, 271);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 43);
+            this.button1.Size = new System.Drawing.Size(130, 35);
             this.button1.TabIndex = 4;
             this.button1.Text = "Closed Window";
             this.button1.UseVisualStyleBackColor = true;
@@ -91,25 +97,37 @@ namespace BatteryMonitor
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(305, 334);
+            this.button2.Location = new System.Drawing.Point(229, 271);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(173, 43);
+            this.button2.Size = new System.Drawing.Size(130, 35);
             this.button2.TabIndex = 5;
             this.button2.Text = "Refresh";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "YO ";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 471);
+            this.ClientSize = new System.Drawing.Size(431, 383);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.batterylife);
             this.Controls.Add(this.strPowerLineStatus);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -127,6 +145,7 @@ namespace BatteryMonitor
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         public System.Windows.Forms.Label strPowerLineStatus;
+        private System.Windows.Forms.Label label3;
     }
 }
 
